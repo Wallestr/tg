df6038e713b03e0647a4107539453eebc1b9f15e
Wallestr <143259318+Wallestr@users.noreply.github.com>
2023-09-12 20:29:08 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,61 @@
+// VANTA.IRDS({
+//     el: "#introo",
+//     mouseControls: true,
+//     touchControls: true,
+//     gyroControls: false,
+//     minHeight: 200.00,
+//     minWidth: 200.00,
+//     scale: 1.00,
+//     scaleMobile: 1.00,
+//     birdSize: 0.70,
+//     wingSpan: 20.00,
+    
+//     quantity: 4.00,
+//     backgroundColor: 0x#fdf4e3  
+    
+//   })
+ 
+
+  VANTA.BIRDS({
+    el: "#introo",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    backgroundColor: 0xFDF4E3 ,
+    color1: 0xf2709c,
+    color2: 0xff9472,
+    birdSize: 1.00,
+    wingSpan: 40.00,
+    speedLimit: 10.00,
+    separation: 100.00,
+    alignment: 100.00,
+    cohesion: 100.00,
+    quantity: 5.00
+  })
+
+  VANTA.BIRDS({
+    el: "#carouselExampleIndicators",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    backgroundColor: 0xFDF4E3 ,
+    color1: 0x008dc3,
+    color2: 0xf9d59d,
+    birdSize: 0.80,
+    wingSpan: 40.00,
+    speedLimit: 10.00,
+    separation: 100.00,
+    alignment: 100.00,
+    cohesion: 100.00,
+    quantity: 5.00
+  })
+
+  
